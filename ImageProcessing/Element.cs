a0ff24c58efadb8205acd4ffcdf0dfdc8a89bb78
Rutmank <rutmank@yandex.ru>
2020-10-27 21:12:58 +0200

    Update

---
+++ ImageProcessing/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing
+{
+    class Element
+    {
+        public Point Point { get; set; } // Класс для точки
+        public Color Color { get; set; } // класс для цвета
+    }
+}
